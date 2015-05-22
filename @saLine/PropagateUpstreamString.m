783ff94116eab4142fbb7af7bfd3ad7845c1254e
Jiang Xin <xingxing993@gmail.com>
2015-05-22 16:29:16 +0800

    Fix saLine Propagate signal bug
    For root level inport it is impossible to set the option ShowPropagatedSignal

--- @saLine/PropagateUpstreamString.m
+++ @saLine/PropagateUpstreamString.m
@@ -6,9 +6,14 @@ if hsrcblk<0
     return;
 elseif ismember(get_param(hsrcblk,'BlockType'),{'Inport','SubSystem'})%Try signal propagate first
     actrec.SetParam(lnhdl,'Name','');
-    actrec.SetParam(hsrcpt,'ShowPropagatedSignals','on');
-    if isempty(get_param(hsrcpt,'PropagatedSignals'))
-        actrec.SetParam(hsrcpt,'ShowPropagatedSignals','off');
+    try
+        actrec.SetParam(hsrcpt,'ShowPropagatedSignals','on');
+        if isempty(get_param(hsrcpt,'PropagatedSignals'))
+            actrec.SetParam(hsrcpt,'ShowPropagatedSignals','off');
+            name_got=obj.Console.GetUpstreamString(hsrcpt);
+            actrec.SetParam(lnhdl,'Name',name_got);
+        end
+    catch
         name_got=obj.Console.GetUpstreamString(hsrcpt);
         actrec.SetParam(lnhdl,'Name',name_got);
     end
