236e461741070ec0905f1b76f6b037437ea194b5
Jiang Xin <xingxing993@gmail.com>
2015-07-10 18:33:45 +0800

    fix problem line name and state (resolve) changed after add_line on an existing line

--- @saRecorder/AddLine_LineToPort.m
+++ @saRecorder/AddLine_LineToPort.m
@@ -9,5 +9,21 @@ dstpos = get_param(dsthdl,'Position');
 % in future the offset may be calculated dynamically
 points_layout = get_line_layout(srchdl, dstpos, [overlap_offset+[-10,0]]);
 parsys = get_param(srchdl,'Parent');
-obj.AddLine(parsys, round(points_layout));
+% backup line info to restore change of state after add_line
+lnname = regexprep(get_param(srchdl, 'Name'), '[<>]', '');
+srcpt = get_param(srchdl,'SrcPortHandle');
+if srcpt>0
+    resolveflg = get_param(srcpt,'MustResolveToSignalObject');
+else
+    resolveflg = 'off';
+end
+% add line
+newln = obj.AddLine(parsys, round(points_layout));
+% restore line state
+if ~isempty(lnname)
+    set_param(newln, 'Name', lnname);
+    if srcpt>0
+        set_param(srcpt,'MustResolveToSignalObject',resolveflg);
+    end
+end
 end
