0d891f8094a3f24d4c73f80efe81f5f268646191
Jiang Xin <xingxing993@gmail.com>
2015-07-10 18:35:31 +0800

    fix problem line state and name change

--- @saRecorder/AddLine_LineToNoSrcLine.m
+++ @saRecorder/AddLine_LineToNoSrcLine.m
@@ -7,5 +7,22 @@ end
 dstpos = get_param(dsthdl,'Points');
 points_layout = get_line_layout(srchdl, dstpos(1,:), overlap_offset);
 parsys = get_param(srchdl,'Parent');
+% backup line info to restore change of state after add_line
+lnname = regexprep(get_param(srchdl, 'Name'), '[<>]', '');
+srcpt = get_param(srchdl,'SrcPortHandle');
+if srcpt>0
+    resolveflg = get_param(srcpt,'MustResolveToSignalObject');
+else
+    resolveflg = 'off';
+end
+% add line
 obj.AddLine(parsys, round(points_layout));
+% restore line state
+if ~isempty(lnname)
+    set_param(newln, 'Name', lnname);
+    if srcpt>0
+        set_param(srcpt,'MustResolveToSignalObject',resolveflg);
+    end
+end
+
 end
