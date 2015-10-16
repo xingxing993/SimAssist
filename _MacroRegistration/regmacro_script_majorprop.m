1773eb2bf035bcafc3244e427a2475f7c932504c
Jiang Xin <xingxing993@gmail.com>
2015-10-16 11:30:23 +0800

    set major prop macro now supports multiple inputs and set in order if multiple blocks selected

--- _MacroRegistration/regmacro_script_majorprop.m
+++ _MacroRegistration/regmacro_script_majorprop.m
@@ -19,17 +19,23 @@ nthprop = n-1;
 if numel(cmdstr)<n
     opt='';
 else
-    opt = cmdstr(n:end);
+    optstr = cmdstr(n:end);
+    opt = regexp(optstr, '\s+|,', 'split');
 end
 
 blks=saFindSystem(gcs, 'block');
 for i=1:numel(blks)
+    if iscellstr(opt)
+        propval = opt{min(i, end)};
+    else
+        propval = opt;
+    end
     btobj = console.MapTo(blks(i));
     if strcmp(btobj.MapKey, '#NO_MATCH#')
         dlgparas = fieldnames(get_param(blks(i), 'DialogParameters'));
-        actrec.SetParam(blks(i), dlgparas{min(end, nthprop)}, opt);
+        actrec.SetParam(blks(i), dlgparas{min(end, nthprop)}, propval);
     else
-        actrec = btobj.SetProperty(blks(i), opt, nthprop);
+        actrec = btobj.SetProperty(blks(i), propval, nthprop);
     end
 end
 success = true;
