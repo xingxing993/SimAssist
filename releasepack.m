45b756861558e16ec2eaabb15aa0942f3d8cb3e5
Jiang Xin <xingxing993@gmail.com>
2015-10-18 15:23:19 +0800

    remove .asv files

--- releasepack.m
+++ releasepack.m
@@ -7,7 +7,7 @@ packdir = ['.\releases\SimAssist_V', datestr(now, 'yyyymmddHHMM')];
 mkdir(packdir);
 
 df = dir;
-copy_exlist = {'releases','demos','releasepack','^\.git'};
+copy_exlist = {'releases','demos','releasepack','^\.git','\.asv$'};
 for i=1:numel(df)
     if ismember(df(i).name, {'.', '..'})
         continue;
@@ -24,6 +24,12 @@ cd(rtdir);
 function pfolder(fd, pcode_exlist)
 dfs = dir(fd);
 ds = dfs([dfs.isdir]);
+%remove .asv files
+asvs = dfs(~cellfun(@isempty, regexp({dfs.name}, '\.asv$', 'once')));
+for i=1:numel(asvs)
+    delete(fullfile(fd, asvs(i).name));
+end
+
 mfs = what(fd);
 mfs = mfs.m;
 for i=1:numel(mfs)
