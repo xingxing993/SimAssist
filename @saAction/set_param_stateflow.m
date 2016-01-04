9ebde3af1a01bd363ce7c10c33aa574deffcc902
Jiang Xin <xingxing993@gmail.com>
2016-01-04 22:02:11 +0800

    Bug fix: handle Stateflow.Event port

--- @saAction/set_param_stateflow.m
+++ @saAction/set_param_stateflow.m
@@ -9,9 +9,9 @@ if isnumeric(varargin{1})
     obj.OldValue = get_param(obj.Handle,'Name');
     sfobj=get_param(obj.Handle,'Object');
     if strcmpi(get_param(obj.Handle,'BlockType'),'Inport')
-        sfdata=rt.find('-isa','Stateflow.Data','Scope','Input','Path',sfobj.Path,'Port',str2double(sfobj.Port));
+        sfdata=rt.find('Scope','Input','Path',sfobj.Path,'Port',str2double(sfobj.Port));
     elseif strcmpi(get_param(obj.Handle,'BlockType'),'Outport')
-        sfdata=rt.find('-isa','Stateflow.Data','Scope','Output','Path',sfobj.Path,'Port',str2double(sfobj.Port));
+        sfdata=rt.find('Scope','Output','Path',sfobj.Path,'Port',str2double(sfobj.Port));
     end
     sfdata.(obj.Property)=obj.NewValue;
 else
