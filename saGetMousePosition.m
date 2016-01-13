a16f338f64077a5fbe3e7bd7e78be8ee42129872
Jiang Xin <xingxing993@gmail.com>
2016-01-13 16:19:49 +0800

    Only add comment

--- saGetMousePosition.m
+++ saGetMousePosition.m
@@ -1,6 +1,6 @@
 function pos = saGetMousePosition
 mousePosition = get(0,'PointerLocation');
-locationBase = get_param(gcs,'Location');
+locationBase = get_param(gcs,'Location'); % Screen Left-Top [0 0], and always gets valid value on different screens
 scrollOffset = get_param(gcs,'ScrollbarOffset');
 screenSize   = get(0,'ScreenSize');
 zoomFactor = str2double(get_param(gcs, 'ZoomFactor'))/100;
